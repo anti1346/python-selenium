84f0751336bfcd397fbc06cf84d62edf6bdd7aa3
aby <anti1346@gmail.com>
2023-01-10 23:02:21 +0900

    no message

--- naversearch.py
+++ naversearch.py
@@ -6,6 +6,9 @@ import datetime
 
 #웹드라이버 설정
 options = webdriver.ChromeOptions()
+# options.add_argument('headless')
+options.add_argument('disable-gpu')
+options.add_argument('lang=ko_KR')
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 driver = webdriver.Chrome(options=options)
 
@@ -20,10 +23,14 @@ element.send_keys(Keys.ENTER)
 time.sleep(7)
 
 # element1 = driver.find_element(By.XPATH, '//*[@id="web_1"]/div[1]/div[2]/div[2]/a')
-element1 = driver.find_element(By.XPATH, '//*[@id="web_1"]/div[1]/div[2]/div[2]/a')
+element1 = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/div[1]/section[2]/div[1]/ul/li/div[1]/div[2]/div[2]/a')
 element1.send_keys(Keys.RETURN)
 time.sleep(10)
 
-element2 = driver.find_element(By.XPATH, '/html/body/div[2]/section/article/div[2]/div[1]/a')
-element2.send_keys(Keys.RETURN)
-time.sleep(10)
+# element2 = driver.find_element(By.XPATH, '/html/body/div/a[1]/span[2]')
+# element2.send_keys(Keys.RETURN)
+# time.sleep(10)
+
+driver.quit()
+
+print('웹 크롤링 완료')
